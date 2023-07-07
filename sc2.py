5ff752e968c33efba250d3c1fb984f32c2645745
Matthew Feifert <matthew.feifert@protonmail.com>
2023-07-07 00:30:01 -0700

    Change variable names for readability

--- sc2.py
+++ sc2.py
@@ -8,29 +8,29 @@ input_option = int(input("Input from:\n1) File\n2) Enter message\n"))
 if input_option == 1:
     input_file = input("File to input: ")
     with open(input_file, 'r') as fr:
-        message_plain = fr.read()
+        message_input = fr.read()
 elif input_option == 2:
-    message_plain = input("Enter message: ")
+    message_input = input("Enter message: ")
 
 random.seed(input("Key: "))
 
 characters = ''.join(random.sample(string.printable,len(string.printable)))
 
-option = int(input("1) Encrypt\n2) Decrypt\n"))
+crypt_option = int(input("1) Encrypt\n2) Decrypt\n"))
 
-message_result = ""
+message_output = ""
 
-if option == 1:
-    for char in message_plain:
-        message_result += characters[string.printable.index(char)]
-elif option == 2:
-    for char in message_plain:
-        message_result += string.printable[characters.index(char)]
+if crypt_option == 1:
+    for char in message_input:
+        message_output += characters[string.printable.index(char)]
+elif crypt_option == 2:
+    for char in message_input:
+        message_output += string.printable[characters.index(char)]
 
-print(message_result)
+print(message_output)
 
-output_option = input("File to output: ")
+output_file = input("File to output: ")
 
-if len(output_option) > 0:
-    with open(output_option, 'w') as fw:
-        fw.write(message_result)
+if len(output_file) > 0:
+    with open(output_file, 'w') as fw:
+        fw.write(message_output)
