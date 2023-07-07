c10ff4eb92c6bf65a5d2b3c37f07cfded11b970b
Matthew Feifert <matthew.feifert@protonmail.com>
2023-07-06 20:35:15 -0700

    Add perens to key prompt for clarity

--- sc1.py
+++ sc1.py
@@ -2,7 +2,7 @@ message_plain = input("Enter Message: ")
 
 characters = ['C', 'Q', 'J', ';', 'l', 'H', '~', 'T', '^', 'R', '&', 'j', 'm', 'v', '1', '?', '!', '5', 't', 'A', '}', 'K', '7', 'i', 'M', '\\', 'a', '%', ':', '3', '#', '*', ',', 'S', 'X', 'b', '.', 'z', 'u', '(', '$', 'D', 's', 'E', 'q', 'Y', 'o', 'r', '{', 'n', 'y', 'V', '>', '0', 'P', '9', ']', ')', 'W', 'h', 'k', 'L', "'", 'x', 'F', 'e', '6', 'N', 'c', 'd', '@', '4', 'g', '-', 'p', '+', 'U', 'w', ' ', '=', 'I', '8', 'f', '<', 'B', '|', '[', '`', '"', 'G', 'Z', 'O', '/', '_', '2']
 
-key = int(input(f"Select Key -{len(characters) - 1} through {len(characters) - 1}: "))
+key = int(input(f"Select Key (-{len(characters) - 1} through {len(characters) - 1}): "))
 
 message_encrypted = ""
 
