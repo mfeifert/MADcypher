bdbbee98e2a9ed82fa29ef8eb4e8e6fd1e09ce7a
Matthew Feifert <matthew.feifert@protonmail.com>
2023-07-07 17:57:34 -0700

    Add input and output options to sc1

--- sc1.py
+++ sc1.py
@@ -2,7 +2,14 @@
 
 import string
 
-message_input = input("Enter Message: ")
+input_option = int(input("Input from:\n1) File\n2) Enter message\n"))
+
+if input_option == 1:
+    input_file = input("File to input: ")
+    with open(input_file, 'r') as fr:
+        message_input = fr.read()
+elif input_option == 2:
+    message_input = input("Enter message: ")
 
 key = int(input(f"Select Key (-{len(string.printable) - 1} through {len(string.printable) - 1}): "))
 
@@ -17,3 +24,9 @@ for char in message_input:
     message_output += string.printable[displace]
 
 print(message_output)
+
+output_file = input("File to output: ")
+
+if len(output_file) > 0:
+    with open(output_file, 'w') as fw:
+        fw.write(message_output)
